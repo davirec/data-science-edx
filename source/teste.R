65d85f4f7781717301dc7cf0e86dd92418017ffc
Davi Simoes <davi.rec@gmail.com>
2019-04-29 14:33:04 -0300

    teste 01

---
+++ source/teste.R
@@ -0,0 +1,16 @@
+# Nesse caso, descreveremos simplesmente que estamos adicionando um novo script. Nesta caixa de diálogo, 
+# o RStudio também fornece um resumo do que você está alterando para o repositório do GitHub. 
+
+# Run
+# 
+# git config --global user.email "you@example.com"
+# git config --global user.name "Your Name"
+# 
+# to set your account's default identity.
+# Omit --global to set the identity only in this repository.
+# 
+# fatal: unable to auto-detect email address (got 'davi@nati01-ubuntu.(none)')
+
+#git config --global user.email "davi.rec@gmail.com"
+
+# git config --global user.name "Davi Simoes"
